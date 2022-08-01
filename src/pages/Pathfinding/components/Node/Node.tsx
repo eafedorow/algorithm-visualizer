f677a26efbcdf997a2ee82d9e3ee49362a169d4f
Evgeniy Alexandrovich <61786122+eafedorow@users.noreply.github.com>
2022-08-01 13:24:25 +0300

    UI update for pathfinding
    UI structure without functionality for pathfinding section

---
+++ src/pages/Pathfinding/components/Node/Node.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import s from './Node.module.scss'
+
+interface Props {
+    isStart: boolean;
+    isEnd: boolean;
+    row: number;
+    col: number;
+    isWall: boolean;
+    onMouseEnter: (nodeType: string, row: number, col: number) => void;
+    onMouseUp: () => void;
+    onMouseDown: (nodeType: string, row: number, col: number) => void;
+    nodeSettingType: string;
+}
+
+export const Node = ({isStart, isEnd, isWall, row, col, onMouseEnter,onMouseDown, onMouseUp, nodeSettingType}: Props) => {
+    const extraClass = isStart ? s.nodeStart : isWall ? s.nodeWall : isEnd ? s.nodeEnd : s.nodeRegular;
+    return (
+        <div
+            onMouseDown={() => { onMouseDown(nodeSettingType, row, col) }}
+            onMouseEnter={() => { onMouseEnter(nodeSettingType, row, col)}}
+            onMouseUp={() => {onMouseUp()}}
+            className={`${s.node} 
+            ${extraClass}`}
+            id={`node-${row}-${col}`
+            }>
+
+        </div>
+    )
+}
